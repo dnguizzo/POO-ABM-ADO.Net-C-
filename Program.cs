b2f84157af9b2d1af0702c7ca79b3b369a077760
dnguizzo <104183590+dnguizzo@users.noreply.github.com>
2022-04-28 14:45:51 -0300

    Add files via upload
    Unidad 2 _ Triangulo

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U2_Ej_Triangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triang1 = new Triangulo();
+            triang1.pA = 3;
+            triang1.pB = 4;
+            triang1.pC = 5;
+
+            Console.WriteLine("Perimetro de un Triangulo de Lados  " + triang1.pA + "," + triang1.pB + "," + triang1.pC);
+            Console.WriteLine("El Perimetro es :  " + triang1.perimetro());
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
---
+++ Triangulo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U2_Ej_Triangulo
+{
+    class Triangulo
+    {
+        //Atributos
+        private int a, b, c;
+
+        //Propiedades
+        public int pA
+        {
+            set { a = value; }
+            get { return a; }
+
+        }
+        public int pB
+
+        {
+            set { b = value; }
+            get { return b; }
+        }
+        public int pC
+        {
+            set { c = value; }
+            get { return c; }
+
+        }
+
+        //Métodos Constructor
+
+        public  Triangulo()
+        {
+            
+        }
+
+
+        //Métodos de control
+
+        public int perimetro()
+        { int per = (a+b+c);
+            return per;
+        }
+
+    }
+}
